0feddcccab4f3209f369f5f6a690e5bafad497ee
Ram Vikram Ganesh <vikrambhai@Rams-MacBook-Air.local>
2024-10-12 07:17:15 +0800

    Node version 22.9.0 and npm version 10.8.3

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ import OnlineEducation from "./pages/OnlineEducation";
 import Entrepreneurship from "./pages/Entrepreneurship";
 import NewsletterForm from './NewsletterForm';
 
+// Create a new QueryClient instance
+const queryClient = new QueryClient();
+
 function App() {
   useEffect(() => {
     // Call API to track page hits
@@ -26,52 +29,29 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Welcome to Jetset Genius</h1>
-      {/* Rest of your main page content */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen">
+            <Navigation />
+            <main className="flex-grow">
+              <h1>Welcome to Jetset Genius</h1>
+              <NewsletterForm />
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/explore-strategy" element={<ExploreStrategy />} />
+                <Route path="/travel-tools" element={<TravelTools />} />
+                <Route path="/sports-hospitality" element={<SportsHospitality />} />
+                <Route path="/online-education" element={<OnlineEducation />} />
+                <Route path="/entrepreneurship" element={<Entrepreneurship />} />
+              </Routes>
+            </main>
+          </div>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
   );
 }
-
-export default App;
-
-function App() {
-  // The page hit logic remains as is
-
-  return (
-    <div className="App">
-      <h1>Welcome to Jetset Genius</h1>
-      <NewsletterForm />
-      {/* Rest of your main page content */}
-    </div>
-  );
-}
-
-export default App;
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <BrowserRouter>
-        <div className="flex flex-col min-h-screen">
-          <Navigation />
-          <main className="flex-grow">
-            <Routes>
-              <Route path="/" element={<Index />} />
-              <Route path="/explore-strategy" element={<ExploreStrategy />} />
-              <Route path="/travel-tools" element={<TravelTools />} />
-              <Route path="/sports-hospitality" element={<SportsHospitality />} />
-              <Route path="/online-education" element={<OnlineEducation />} />
-              <Route path="/entrepreneurship" element={<Entrepreneurship />} />
-            </Routes>
-          </main>
-        </div>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
 
 export default App;
